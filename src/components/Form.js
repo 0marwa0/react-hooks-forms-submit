a46866a47d961cd1ca4c38e49a59889b67694007
marwa <marwajawad19@gmail.com>
2021-09-11 19:04:04 +0300

    adding validation to the  input

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,15 +13,22 @@ function Form() {
     setLastName(event.target.value);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+const [errors, setErrors] = useState([]);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // first name is required
+  if (firstName) {
     const formData = { firstName: firstName, lastName: lastName };
     const dataArray = [...submittedData, formData];
     setSubmittedData(dataArray);
     setFirstName("");
     setLastName("");
+    setErrors([]);
+  } else {
+    setErrors(["First name is required!"]);
   }
-
+} 
   const listOfSubmissions = submittedData.map((data, index) => {
     return (
       <div key={index}>
